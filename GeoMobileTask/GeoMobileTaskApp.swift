29511182aea70f7935a1f026a9c12a11281f12dd
abdul ahad <abdul_ahad1996@live.com>
2023-05-16 00:07:25 +0200

    Initial Commit

---
+++ GeoMobileTask/GeoMobileTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoMobileTaskApp.swift
+//  GeoMobileTask
+//
+//  Created by macbook abdul on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GeoMobileTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
