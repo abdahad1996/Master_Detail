5a0398c0a2da2b08b7042ae005a95f493a3491d3
abdul ahad <abdul_ahad1996@live.com>
2023-05-16 12:29:14 +0200

    - added userMapper

---
+++ GeoMobileTask/Apis/User Api/UserMapper.swift
@@ -0,0 +1,54 @@
+//
+//  UserMapper.swift
+//  GeoMobileTask
+//
+//  Created by macbook abdul on 16/05/2023.
+//
+
+import Foundation
+enum UserMapper {
+    struct UserRootResponse: Codable {
+        let data: [UserRemote]
+        
+        private enum CodingKeys: String, CodingKey {
+            case data = "Data"
+        }
+        
+        var users : [User] {
+            data.map({
+                User(id: $0.id, email: $0.email, firstName: $0.firstName, lastName: $0.lastName, avatar: $0.avatar)
+            })
+        }
+    }
+    
+    
+    struct UserRemote: Codable {
+        let id: Int
+        let email, firstName, lastName: String
+        let avatar: String
+
+        enum CodingKeys: String, CodingKey {
+            case id, email
+            case firstName = "first_name"
+            case lastName = "last_name"
+            case avatar
+        }
+    }
+    
+    private enum Error: Swift.Error {
+        case invalidData
+    }
+
+    private static var OK_200: Int { 200 }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [User] {
+        guard response.statusCode == OK_200,
+              let items = try? JSONDecoder().decode(UserRootResponse.self, from: data) else {
+            throw Error.invalidData
+        }
+        
+        return items.users
+    }
+}
+
+ 
